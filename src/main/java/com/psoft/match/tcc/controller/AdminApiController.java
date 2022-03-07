125cc572dd504a4b00318f47e548aa044a19792c
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2022-03-07 17:41:10 -0300

    refactor: mudando do endpoint /professor para /admin

--- src/main/java/com/psoft/match/tcc/controller/AdminApiController.java
+++ src/main/java/com/psoft/match/tcc/controller/AdminApiController.java
@@ -4,32 +4,34 @@ import com.psoft.match.tcc.dto.ProfessorDTO;
 import com.psoft.match.tcc.model.user.Professor;
 import com.psoft.match.tcc.service.ProfessorService;
 import com.psoft.match.tcc.util.Constants;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(Constants.BASE_API_ENDPOINT + "/professor")
+@RequestMapping(Constants.BASE_API_ENDPOINT + "/admin")
 @CrossOrigin
-public class ProfessorApiController {
+public class AdminApiController {
 
     @Autowired
     private ProfessorService professorService;
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/create/professor")
+    @ApiOperation(value = "Criação de um novo professor")
     public ResponseEntity<Professor> createProfessor(@RequestBody ProfessorDTO professorDTO) {
         Professor createdProfessor = professorService.createProfessor(professorDTO);
         return new ResponseEntity<>(createdProfessor, HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/update/professor/{id}")
     public ResponseEntity<Professor> updateProfessor(@PathVariable Long id, @RequestBody ProfessorDTO professorDTO) {
         Professor updatedProfessor = professorService.updateProfessor(id, professorDTO);
         return new ResponseEntity<>(updatedProfessor, HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/delete/professor/{id}")
     public ResponseEntity<Void> deleteProfessor(@PathVariable Long id) {
         professorService.deleteProfessor(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
