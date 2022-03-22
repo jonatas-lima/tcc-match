eede65d2ebebe0a7283013d77b63320083ec446e
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2022-03-22 17:16:00 -0300

    fix: removendo codigo nao usado

--- src/main/java/com/psoft/match/tcc/config/SystemConfig.java
+++ src/main/java/com/psoft/match/tcc/config/SystemConfig.java
@@ -2,15 +2,11 @@ package com.psoft.match.tcc.config;
 
 import com.psoft.match.tcc.service.db.DBService;
 import com.psoft.match.tcc.service.email.EmailService;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 public interface SystemConfig {
 
     EmailService emailService();
 
     DBService dbService();
-
-//    MailSender mailSender();
 
 }
